ab62fb17aa0076facc7b753429481dec230f902e
aminya <aminyahyaabadi74@gmail.com>
2020-01-26 04:16:29 -0600

    removing old function

--- src/function_description.jl
+++ src/function_description.jl
@@ -23,67 +23,6 @@ function recursive_search!(in)
 end
 
 function description_parse()
-
-    include("fileslist/xmlfileslist.jl")
-    xmlfiles = xmlfiles[20:21]
-
-    numfile = length(xmlfiles)
-    docs = Vector{Any}(undef, numfile)
-    groups = Vector{String}(undef, numfile)
-    types = Vector{Integer}(undef, numfile)
-
-    for (i, file) in enumerate(xmlfiles)
-        groups[i] = match(r"input\/help\/(.+)\/", file).captures[1]
-        xml = readxml(file)
-        try
-            # @infiltrate
-            docs[i] = Doc2(xml)
-            types[i] = 2
-        catch
-            docs[i] = Doc(xml)
-            types[i] = 1
-        end
-
-    end
-    dict = Dict(g => d for (d, g) in zip(docs, groups))
-
-    funs_docs = Vector{Fun}(undef, numfile)
-    for (d, t) in zip(docs, types)
-        if t == 1
-            funs_docs[i] = reduce(vcat,
-                                StructArray(
-                                        reduce(vcat,
-                                            StructArray(
-                                                StructArray(
-                                                    d.toc
-                                                ).help
-                                            ).groups
-                                        )
-                                ).funs,
-                            )
-        else t == 2
-            funs_docs[i] = StructArray(
-                                reduce(vcat,
-                                    StructArray(
-                                            reduce(vcat,
-                                                StructArray(
-                                                    StructArray(
-                                                        d.toc
-                                                    ).help
-                                                ).groups
-                                            )
-                                    ).subgroup
-                                )
-                            ).funs
-        end
-    end
-
-    return dict, docs, funs_docs
-end
-
-# description_dict, docs, funs_docs = description_parse()
-
-function description_parse2()
     global allnodes
     allnodes = Node[]
     include("fileslist/xmlfileslist.jl")
@@ -105,4 +44,4 @@ function description_parse2()
     return dict
 end
 
-funsdict = description_parse2()
+funsdict = description_parse()
